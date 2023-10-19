3787ebb4b5d0ebb1e60eeee0c701df9cde89aa4c
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:27:25 +0530

    allassignmemtoperators

---
+++ assignmentoperator.py
@@ -0,0 +1,27 @@
+number1 = 10
+number2 = 20
+print(number1)
+print(number2)
+number1 &= number2
+print(" bitwiseAND = ",number1)
+number1 |= number2
+print("bitwiswOR = ", number1)
+number1 >>= number2
+print("bitwiseright = ", number1)
+number1 <<= number2
+print("bitwiseleft = ", number2)
+result = number1 + number2
+number1 += number2
+print("addition =", number1)
+number1 -= number2
+print("subtraction =", number2)
+number1 *= number2
+print("multiplication = ",number1)
+number1 /= number2
+print("divison = ", number1)
+number1 %= number2
+print("modulus =", number1)
+number1 //= number2
+print("floor divison =", number1)
+number1 **= number2
+print("exponent =", number1)
