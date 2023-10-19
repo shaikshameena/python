09ba9ae366d9f4d819e9de8bd07b9dd999d1003e
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 14:57:53 +0530

    accessingstringelememts

---
+++ accessingstringele.py
@@ -0,0 +1,14 @@
+string1 = 'hello'
+string2 = ''' qualizeal 
+              quality is
+              core'''
+string3 = "abdr"
+print(string1[-1])
+print(string1[3])
+print(string2[6])
+print(string2[8])
+print(string3[3])
+#print(string1[5])
+for character in string2 :
+    print([character])
+print(string1[5])
