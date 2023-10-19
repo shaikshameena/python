0e02443a431f4c842abc361ff61b18bcfa40345d
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:40:17 +0530

    divisonoperations

--- divisonoperator.py
+++ divisonoperator.py
@@ -8,13 +8,14 @@ print(- 5 // 2)
 print( 5 // 2)
 print (5.0/2)
 print (-5.0/2)
+#division operator on Boolean values
 class MyClass:
 	def __init__(self, value):
 		self.value = value
 
 	def __truediv__(self, other):
 		return MyClass(self.value and other.value)
-
+ 
 number1 = MyClass(True)
 number2 = MyClass(False)
 result = number1 / number2 # c.value is False
