0ef362bde87c0bc9a82e9a2dd7fa5b976e46d63c
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 19:11:54 +0530

    stringslice

---
+++ stringslice.py
@@ -0,0 +1,10 @@
+string1 = "hello"
+string2 = "world, quality"
+print(string1[0 : 3])
+print(string2[- 1 : 4])
+print(string2[len(string2) - 1 : 4])
+string3 = "abdr"
+print(string3[- 3 : -2])
+print(string3[len(string3) - 3 : len(string3) - 2])
+print(string2[ :: - 1])
+print(string2[0 : len(string2) - 1])
