a90730693b9cce960c1e7a1656e6ee40369d0f4a
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 18:22:09 +0530

    deletingitemsinset

---
+++ popset.py
@@ -0,0 +1,12 @@
+variable = {10, 30, "abcd", 10, 20}
+print(variable)
+print(variable.pop())
+print(variable)
+print(variable.pop())
+print(variable)
+variable1 = {20, 40, 50, "cdef", 78, 7.9}
+print(variable1)
+print(variable1.remove(20))
+print(variable1)
+variable1.clear()
+print(variable1)
