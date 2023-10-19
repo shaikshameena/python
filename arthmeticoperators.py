8e668855a1c8d700afd391ed322b1ffc08f87312
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:26:12 +0530

    arithmeticoperationsonumbers

---
+++ arthmeticoperators.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 20
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+modulus = number1 % number2
+exponent = number1 ** number2
+floordivison = number1 // number2
+print("addition = ", addition)
+print("subtraction = ", subtraction)
+print("multiplication = ", multiplication)
+print("divison = ", division)
+print("modulus = ", modulus)
+print("exponent = ", exponent)
+print("floor divison = ", floordivison)
