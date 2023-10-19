468366a600337189a2234966057ef06078ecf5d8
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:24:44 +0530

    arbitaryargumentsfunction

---
+++ arbitaryarg.py
@@ -0,0 +1,11 @@
+# *args for variable number of arguments
+def myfun(*argv) :
+    for arg in argv :
+        print(arg)
+myfun("hello", "hi", "goodmorning")
+
+# *kwargs for variable number of keyword arguments
+def myfun(**kwargs) :
+    for key, value in kwargs.items():
+        print("%s = %s " %(key, value))
+myfun(first = "i am ", mid = "shameena", last = "working in qualizeal")
