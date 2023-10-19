be5a125a5a67108619278d579e0a1a01b7083d0a
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:41:23 +0530

    documentstring

---
+++ docstring.py
@@ -0,0 +1,7 @@
+def evenorodd(number):
+    """Function to check if the number is even or odd"""
+    if(number % 2 == 0):
+        print("even")
+    else:
+        print("odd")
+print(evenorodd.__doc__)
