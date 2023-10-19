38a540c62dccabf2762c82ec9bd8df7432f0ffb6
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:12:43 +0530

    addingtwonumberusingfunctions

---
+++ addingtwonumusingfun.py
@@ -0,0 +1,7 @@
+def addition(number1 : int, number2 : int) -> int :
+    result = number1 + number2
+    return result
+  
+number1, number2 = 19, 12
+result = addition(number1, number2)
+print("addition of two numbers = ", result)
