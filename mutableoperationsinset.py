a5e49fcafc03eca945046eca1b9290604738ee6d
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 18:08:01 +0530

    mutableoperationsinset

---
+++ mutableoperationsinset.py
@@ -0,0 +1,8 @@
+variable = {10, "abcd", 7.8, "vcd", 21}
+print(variable)
+variable.add("abdr")
+print(variable)
+variable.update("sham","123")
+print(variable)
+variable.update(["3","shame"])
+print(variable)
