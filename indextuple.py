9ff54684f79e837edcc5e8168f3bf7b622fdd609
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 17:44:40 +0530

    printingitemofindex

---
+++ indextuple.py
@@ -0,0 +1,9 @@
+tuple1 = (10, 20, 30, 40, 40, "ab", "cd", 7.9)
+print(tuple1)
+index = tuple1[3]
+print(index)
+index1 = tuple1.index(40)
+print(index1)
+tuple3 = ('a', 'b', 'c', 'd', 'e', 'f', 'g','a')
+index2 = tuple3.index('a', 3, 8)
+print(index2)
