fe80223c4fc7ac1258da72bde208f4a1f58c3bd0
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 18:52:55 +0530

    realtionaloperations

---
+++ relationaloperators.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 20
+greater = number1 > number2
+lesser = number1 < number2
+equalto = number1 == number2
+notequal = number1 != number2
+greaterorequal = number1 >= number2
+lesserorequal = number1 <= number2
+print("greater = ", greater)
+print("lesser = ", lesser)
+print("equalto =", equalto)
+print("notequal = ", notequal)
+print("greaterorequal = ", greaterorequal)
+print("lesserorequal = ", lesserorequal)
