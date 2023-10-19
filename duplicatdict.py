a1fb228fa96af97b86177b7f9d19c70479cffe9a
shaikshameena <shaikshameena003@gmail.com>
2023-10-19 15:44:01 +0530

    duplicatekeyandvaluesindict

---
+++ duplicatdict.py
@@ -0,0 +1,8 @@
+dict = {"name" : "abdr", "age" : 23, "username" : "abdr"}
+print(dict)
+dict1 = {"name" : "sham"}
+print(dict1)
+dict2 = {"Name" : "shame", "age" : 23, "Name" : "sham"}
+print(dict2)
+dict3 = {"username" : "sham", "age" : 23, "username" : "sham"}
+print(dict3)
