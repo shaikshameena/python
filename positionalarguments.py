afc78baae4a52fe13faf122458ed2b802bcde122
shaikshameena <shaikshameena003@gmail.com>
2023-10-20 20:42:32 +0530

    positional argument function

--- positionalarguments.py
+++ positionalarguments.py
@@ -2,6 +2,6 @@ def nameage(name, age):
     print("my name is ", name)
     print("my age is", age)
 print("case - 1")
-nameage("shameena", 25)
+nameage("shameena", 25) # passing arguments as parameters declaration
 print("\ncase - 2")
-nameage(25,"shameena")
+nameage(25,"shameena") # changing position
