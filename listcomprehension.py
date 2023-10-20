b8d0c57b7a6c85d9ab7487f0e939c8bfef1a946e
shaikshameena <shaikshameena003@gmail.com>
2023-10-20 20:22:54 +0530

    list comprehnesion

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,8 +1,9 @@
+# list comprehension
 list1 = [10, 20, 30, 40, 50, 60]
 print(list1)
 squared = [ x ** 2 for x  in list1]
 print(squared)
 list2 = [list1 for list1 in [10, 20, 30]]
 print(list2)
-list3 = ["even number" if i % 2 == 0 else "odd number" for i in range(10)]
+list3 = ["even number" if i % 2 == 0 else "odd number" for i in range(10)] 
 print(list3)
