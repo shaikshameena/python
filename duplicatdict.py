6dcd9fae34179d642916e3c25fced6b7fec16274
shaikshameena <shaikshameena003@gmail.com>
2023-10-20 18:03:11 +0530

    duplicatekey using in dict

--- duplicatdict.py
+++ duplicatdict.py
@@ -1,6 +1,6 @@
 dict = {"name" : "abdr", "age" : 23, "username" : "abdr"}
 print(dict)
-dict1 = {"name" : "sham"}
+dict1 = {"name" : "sham"} # using  same key again
 print(dict1)
 dict2 = {"Name" : "shame", "age" : 23, "Name" : "sham"}
 print(dict2)
