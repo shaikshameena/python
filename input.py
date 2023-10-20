14930a2beb7a6b680d42e48bc1cee275a04c623c
shaikshameena <shaikshameena003@gmail.com>
2023-10-20 19:47:20 +0530

    using of inputfunction

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 name = input("enter your name:")
 print(name)
-number1 = input("enter first number:")
+number1 = input("enter first number:") # input function
 number2 = input("enter second number:")
 print( number1 + number2)
 
