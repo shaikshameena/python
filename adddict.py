a6475913a9fc7ca62938126dbc4e863677d58b12
shaikshameena <shaikshameena003@gmail.com>
2023-10-21 08:52:02 +0530

    initialize key and values in exist dict

--- adddict.py
+++ adddict.py
@@ -1,5 +1,5 @@
 # adding key and values in exist dictionary
 variable = {"name" : "abdr", "age" : 23}
 print(variable)
-variable1 = {"name" : "sham"}
+variable1 = {"name" : "sham"} # exist dict
 print(variable1)
