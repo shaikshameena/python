cbfd54a821e7fa3f08b5256081b0f55aa7bddd09
shaikshameena <shaikshameena003@gmail.com>
2023-10-21 12:55:45 +0530

    counting items in tuple

--- countitemintuple.py
+++ countitemintuple.py
@@ -1,7 +1,7 @@
 # count items
 tuple1 = (10, 20, 30, 40, 50)
 print(tuple1)
-count = tuple1.count(20)
+count = tuple1.count(20) #count function
 print(count)
 tuple2 = (10, 10, 20, "ab", 3, 4, 3)
 print(tuple2)
