10b6ef29fbf90dbb4ba7dacf5e83feadbcae9c64
shaikshameena <shaikshameena003@gmail.com>
2023-10-21 13:14:39 +0530

    duplicate items in list

--- duplitateitemsinlist.py
+++ duplitateitemsinlist.py
@@ -1,4 +1,4 @@
-list1 = [10, 20, 30, "adbd", 10, 20] # adding duplicate items
+list1 = [10, 20, 30, "adbd", 10, 20]  # adding duplicate items
 print(list1)
 list2 = [1, 2, 3, 4, "a", "b"]
 print(list2)
