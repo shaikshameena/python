c5314796b6af52f7933c5fd6004264fc84af03e8
shaikshameena <shaikshameena003@gmail.com>
2023-10-21 12:23:09 +0530

    any function

--- anyallfun.py
+++ anyallfun.py
@@ -2,7 +2,7 @@
 list1 = []
 list2 = []
 for initial in range(1, 11) :
-    list1.append(4 * initial)
+    list1.append(4 * initial) # append function 
 for initial in range(0, 10):
     list2.append(list1[initial] %5 == 0)
 print('See whether at least one number is divisible by 5 in list 1=>')
