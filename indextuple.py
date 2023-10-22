e8cb8fb8f6c1c73ff15a60cd3e84bb240b0ab32c
shaikshameena <shaikshameena003@gmail.com>
2023-10-22 07:42:12 +0530

    value of each index

--- indextuple.py
+++ indextuple.py
@@ -1,6 +1,6 @@
 tuple1 = (10, 20, 30, 40, 40, "ab", "cd", 7.9)
 print(tuple1)
-index = tuple1[3] # printing value of in index 3
+index = tuple1[3] #printing value of in index 3
 print(index)
 index1 = tuple1.index(40)
 print(index1)
