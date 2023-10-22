e686874ae66b9e847d6af28c55f198a7f9f5caed
shaikshameena <shaikshameena003@gmail.com>
2023-10-22 07:58:52 +0530

    math operations on set

--- mathset.py
+++ mathset.py
@@ -1,11 +1,11 @@
 variable1 = {10, 20, 30, 49, "abdr", 9.8}
 print(variable1)
-variable2 = {10, 2, 30, 49, "abdr", 9.8,"sham",7,5}
+variable2 = {10, 2, 30, 49, "abdr", 9.8,"sham", 7, 5}
 print(variable2)
 print(variable1.union(var2))
 variable3 = {1, 2, 3, 49, "abdr", 9.8}
 print(variable3)
-variable4 = {10, 20, 30, 49, "abdr", 9.8,"sham",7,5}
+variable4 = {10, 20, 30, 49, "abdr", 9.8, "sham", 7, 5}
 print(variable4)
 print(variable3.intersection(var4))
 print(variable1 & variable3)
